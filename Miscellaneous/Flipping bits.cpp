f9ad568cb6e23c88e8d240246a2818241793ae5e
sanchit garg <sanchit2812@gmail.com>
2019-06-19 20:01:00 +0530

    Create Flipping bits.cpp

---
+++ Miscellaneous/Flipping bits.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    long int t,n,x=4294967295;
+    cin >> t;
+    while(t--)
+    {
+        cin>> n;
+        cout<<x-n<<'\n';
+    }
+    return 0;
+}
